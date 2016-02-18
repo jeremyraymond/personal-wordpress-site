6069b061d816148f7a434a959afbf1afdbd59f89
jeremyraymond <jeremy.r.raymond@gmail.com>
2016-02-18 04:44:03 -0500

    Almost finished header
    The header is almost finished except for the What I Do link

--- wp-content/themes/jeremy-raymond/header.php
+++ wp-content/themes/jeremy-raymond/header.php
@@ -42,26 +42,11 @@
 
 		<?php tha_header_top(); ?>
 
-			<section class="site-branding">
-				<div class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
+			<section class="site-branding section">
+				<div class="site-title"><?php bloginfo( 'name' ); ?></div>
 				<div class="site-description"><?php bloginfo( 'description' ); ?></div>
+				<a class="aboutme-link" href="#aboutme">WHAT I DO</a>
 			</section>
-
-			<nav id="primary-nav" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
-				<button class="menu-button">
-					<span class="dashicons <?php echo get_theme_mod( 'some_like_it_neat_mobile_nav_icon', 'dashicons-menu' ); ?>"></span><?php echo get_theme_mod( 'some_like_it_neat_mobile_nav_label', 'Menu' ); ?>
-				</button>
-		        <?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'primary-navigation',
-							'menu_class' => 'flexnav', //Adding the class for FlexNav
-							'items_wrap' => '<ul data-breakpoint=" '. esc_attr( get_theme_mod( 'some_like_it_neat_mobile_min_width', '768' ) ) .' " id="%1$s" class="%2$s">%3$s</ul>', // Adding data-breakpoint for FlexNav
-						)
-					);
-				?>
-
-			</nav><!-- #site-navigation -->
 
 			<?php tha_header_bottom(); ?>
 
